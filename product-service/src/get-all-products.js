e470509c5f2af4a5f58826357cab4ef7f0b511ef
Iryna Dunets <idunets@yahoo.com>
2020-11-09 16:50:12 +0200

    Update get-all-products.js

--- product-service/src/get-all-products.js
+++ product-service/src/get-all-products.js
@@ -3,10 +3,15 @@ import { createClient } from './db/connect';
 export const getAllProducts = async (event) => {
   try {
     const client = await createClient();
+
     console.log('getAllProducts');
-    const { rows: products } = await client.query(`select * from product`);
+    console.log(event);
+    const { rows: products } = await client.query(`SELECT p.id, p.title, p.description, p.price, s.count
+        FROM product AS p JOIN stock AS s ON p.id = s.product_id`);
     console.log(products);
+    
     client.end();
+
   return {
     statusCode: 200,
     body: JSON.stringify(products)
