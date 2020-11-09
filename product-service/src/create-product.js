c08abcfa8127490dc785b265a7ed9989a20e5c5b
Iryna Dunets <idunets@yahoo.com>
2020-11-09 16:49:38 +0200

    Update create-product.js

--- product-service/src/create-product.js
+++ product-service/src/create-product.js
@@ -2,29 +2,28 @@ import { createClient } from './db/connect';
 
 export const createProduct = async event => {
   try {
-    console.log('Event Path Parameters', event.body);
     const client = await createClient();
-    const requestBody = JSON.parse(event.body);
 
-    const {  title, description, price, count} = requestBody;
-
+    const { title, description, price, count } = JSON.parse(event.body);
+    console.log(event.body);
+    console.log(title);
     if (
       typeof description !== 'string' || typeof title !== 'string' || typeof price !== 'number' || typeof count !== 'number'
     ) {
       return {
         statusCode: 400,
-        body: 'Bad input provided in product fields'
+        body: 'Input data is invalid'
       };
     }
 
-    const result1 = await client.query(`INSERT INTO product (title, description, price) VALUES (${title}, ${description}, ${price}) RETURNING id`);
-    const productId = result1.rows[0].id;
-    await client.query(`INSERT INTO stock (product_id, count) VALUES (${productId}, ${count})`);
+    const product = await client.query('INSERT INTO product (title, description, price) VALUES ($1, $2, $3) RETURNING id',[title, description, price]);
+    const productId = product.rows[0].id;
+    await client.query('INSERT INTO stock (product_id, count) values ($1, $2)',[productId, count]);
+
     client.end();
 
     return {
-      statusCode: 200,
-      body: JSON.stringify({ product: {description, title, price, count, id: productId } }),
+      statusCode: 200
     };
   } catch (e) {
     return {
@@ -33,3 +32,4 @@ export const createProduct = async event => {
     };
   }
 };
+
