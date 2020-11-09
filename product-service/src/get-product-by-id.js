a48493215f947f773ecb20552b3114d823ed5cfa
Iryna Dunets <idunets@yahoo.com>
2020-11-09 16:51:08 +0200

    Update get-product-by-id.js

--- product-service/src/get-product-by-id.js
+++ product-service/src/get-product-by-id.js
@@ -2,26 +2,28 @@ import { createClient } from './db/connect';
 
 export const getProductById = async (event) => {
 
-try {
-  const client = await createClient();
-    const { id } = event.pathParameters;
-    console.log(id);
-    const dbResponse = await client.query(`SELECT product.id, product.title, product.description, product.price
-    from  product where product.id = ${id}`);
-    const product = dbResponse.rows[0];
+  try {
+    const client = await createClient();
 
+    console.log('getProductById');
+    const productId = event.path.split('/')[2];
+    const { rows: products } = await client.query(`SELECT p.id, p.title, p.description, p.price, s.count
+        FROM product AS p JOIN stock AS s ON p.id = s.product_id`);
+    console.log(products);
+    const item = products.find(el => el.id === event.path.split('/')[2]);
+    console.log(item);
+    
     client.end();
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ product }),
-    };
+  return {
+    statusCode: 200,
+    body: JSON.stringify({product:item}, null, 2),
+  };
   } catch (e) {
-
-    return {
-      statusCode: 500,
-      body: 'Error while reading data',
-    };
-    next(e);
+  return {
+    statusCode: 500,
+    body: 'Error while reading data',
+  };
+     next(e);
   }
-};
+  };
